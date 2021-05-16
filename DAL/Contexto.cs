f683ab23c052a144bb09eb6ee35d78e180ba5484
Raldy Steven Lopez Gilbert <raldy_lopez@ucne.edu.do>
2021-05-15 23:56:47 -0400

    Agregar archivos de proyecto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea2.Models;
+
+namespace Tarea2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Personas.db");
+        }
+    }
+}
